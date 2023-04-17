bc90cfbc520628462bf9cc5b7a943f7e64870517
imvickykumar999 <imvickykumar999@gmail.com>
2023-04-17 22:48:07 +0530

    app.app_context().push()
    This typically means that you attempted to use functionality that needed
    the current application. To solve this, set up an application context
    with app.app_context(). See the documentation for more information.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.app_context().push()
 db_name = "auth.db"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{db}'.format(db=db_name)
--- create_db.py
+++ create_db.py
@@ -1,3 +1,5 @@
 
+# https://stackoverflow.com/a/44944205/11493297
+
 from app import create_db
 create_db()
